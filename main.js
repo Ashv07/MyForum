8759592cee0fd18f62b3a281e2a14b54c739ae8a
Ashutosh Sharma <ashu.hrl8@gmail.com>
2021-07-23 01:44:54 +0530

    Final layout

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // Get the modal
-var modal1 = document.getElementById("myModal1");
-var modal2 = document.getElementById("myModal2");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
-var btn3 = document.getElementById("myBtn3");
+var btn3 = document.getElementById("c-btn");
 var btn4 = document.getElementById("like");
 var btn5 = document.getElementById("follow");
 
@@ -14,27 +13,22 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  if (modal1.style.display == "block") {
-    modal1.style.display = "none";
-  }
-  else if(modal2.style.display == "block") {
-    modal2.style.display = "none";
-  }
+  modal.style.display = "none";
 };
 
 
 // When the user clicks the button, open the modal
 btn1.onclick = function () {
-  modal1.style.display = "block";
+  modal.style.display = "block";
 };
 
 btn2.onclick = function () {
-  modal1.style.display = "block";
+  modal.style.display = "block";
 };
 
-// For Sign-up button
+// For comment button
 btn3.onclick = function () {
-    modal2.style.display = "block";
+    modal.style.display = "block";
 };
 
 // For like button
@@ -48,13 +42,9 @@ btn5.onclick = function () {
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
-  else if(event.target == modal2)
-  {
-    modal2.style.display = "none"
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
   }
 };
 
@@ -89,25 +79,25 @@ function showComments() {
     commentsObj = JSON.parse(comments);
   }
   let html1 = `<div class="comments">
-  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User113</a></p>
-  <p class="bold">My favourite movie is Avengers: Infinity War. It has a very gripping story with non-stop action.</p>
-</div>
-<div class="comments">
-  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User23</a></p>
-  <p class="bold">I love The Shawshank Redemption. Everyone gave a brilliant performance in that movie.</p>
-</div>
-<div class="comments">
-  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User420</a></p>
-  <p class="bold">I love watching Harry Potter series. I grew up watching those movies and now I'm thinking of reading the novels.</p>
-</div>
-<div class="comments">
-  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User200</a></p>
-  <p class="bold">The Dark Knight is one of the best movie ever made. Heath Ledger gave a legendary performance in that movie.</p>
-</div>
-<div class="comments">
-  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User007</a></p>
-  <p class="bold">I just love watching Bond movies.</p>
-</div>`;
+                  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User113</a></p>
+                  <p class="bold">My favourite movie is Avengers: Infinity War. It has a very gripping story with non-stop action.</p>
+               </div>
+              <div class="comments">
+                  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User23</a></p>
+                  <p class="bold">I love The Shawshank Redemption. Everyone gave a brilliant performance in that movie.</p>
+               </div>
+               <div class="comments">
+                  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User420</a></p>
+                  <p class="bold">I love watching Harry Potter series. I grew up watching those movies and now I'm thinking of reading the novels.</p>
+               </div>
+               <div class="comments">
+                  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User200</a></p>
+                  <p class="bold">The Dark Knight is one of the best movie ever made. Heath Ledger gave a legendary performance in that movie.</p>
+               </div>
+               <div class="comments">
+                  <p class="bold">By&nbsp;<a href="#"><i class="fa fa-user"></i></a><a href="#" class="user_id">User007</a></p>
+                  <p class="bold">I just love watching Bond movies.</p>
+               </div>`;
   let html2 = "";
   commentsObj.forEach(function (element, _index) {
     html2 += `
